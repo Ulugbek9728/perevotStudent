f86b890fe4f816cd634805322097b0a067ec7a51
Sarvarbek <mukhtarovsarvarbek@gmail.com>
2023-07-12 17:36:24 +0500

    ariza add rules

--- src/pages/ariza.js
+++ src/pages/ariza.js
@@ -1,9 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {useTranslation} from "react-i18next";
 import "../asset/arizaPage.scss"
-import axios from "axios";
-import {Button, Form, Segmented, Select} from 'antd';
-import {ApiName1} from "../APIname1";
+import {Button, Form, Input, Segmented, Select} from 'antd';
 import {useNavigate} from "react-router";
 import {toast} from "react-toastify";
 import Nav from "../component/nav";
@@ -32,13 +30,19 @@ function Ariza(props) {
             city: "",
             district: "",
             phone: "",
-            educationForm: '',
-            educationLang: '',
-            educationType: '',
+            oldEducationForm: '',
+            newEducationForm: '',
+            oldEducationLang: '',
+            newEducationLang: '',
+            oldEducationType: '',
+            newEducationType: '',
             applicationType: 'CHANGE_SPECIALITY',
             attachList: [],
             changeLanguage: '',
             changeSpecialtyID: null,
+            reasonFileId: null,
+            reasonText: '',
+            passportPhotoId: null,
         });
     const [file, setFile] = useState([{
         fileName: '',
@@ -52,7 +56,6 @@ function Ariza(props) {
     const [specialityList, setSpecialityList] = useState([]);
     const [specialityLang, setSpecialityLang] = useState([]);
     const [specialityForms, setSpecialityForms] = useState([]);
-
 
     useEffect(() => {
         getStudent();
@@ -91,9 +94,9 @@ function Ariza(props) {
                     country: response.data.data.country.name,
                     city: response.data.data.province.name,
                     district: response.data.data.district.name,
-                    educationForm: response.data.data.educationForm.name,
-                    educationLang: response.data.data.educationLang.name,
-                    educationType: response.data.data.educationType.name,
+                    oldEducationForm: response.data.data.educationForm.name,
+                    oldEducationLang: response.data.data.educationLang.name,
+                    oldEducationType: response.data.data.educationType.name,
                 });
                 getAlternativePublicList(response?.data?.data?.specialty?.name)
                     .then((res) => {
@@ -145,6 +148,7 @@ function Ariza(props) {
     const handleChangeLen = (value) => {
         setStudent({...Student, changeLanguage: value});
     };
+
     useEffect(() => {
         const languages = specialityList
             ?.filter((item) => item.id === Student?.changeSpecialtyID)
@@ -167,8 +171,6 @@ function Ariza(props) {
             }
         }));
     }, [Student?.changeSpecialtyID, specialityList])
-
-    console.log(Student)
 
     const showForm = () => {
         // eslint-disable-next-line default-case
@@ -237,21 +239,46 @@ function Ariza(props) {
             }
             case "CHANGE_SPECIALITY": {
                 return (
-                    <Form name="dynamic_form_nest_item"
-                          onFinish={onFinish}
-                          autoComplete="off">
-                        <p>{t('direction')}:</p>
-                        <Form.Item>
+                    <Form
+                        name="dynamic_form_nest_item"
+                        onFinish={onFinish}
+                        autoComplete="off"
+                        fields={[
+                            {
+                                name: 'phone',
+                                value: Student?.phone
+                            }
+                        ]}
+                    >
+
+                        <Form.Item
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Telefon raqam kiritilishi shart!',
+                                    whitespace: true,
+                                    max: 12,
+                                    min: 12
+                                }
+                            ]}
+                            name="phone"
+                        >
                             <p>{t('phone')}:
-                                <br/>
-                                <input onChange={(e) => {
-                                    setStudent({
-                                        ...Student, phone: e.target.value
-                                    })
-                                }}
-                                       className='form-control' type="number"/>
+                                <Input
+                                    placeholder="Telefon raqam kiriting..."
+                                    name="phone"
+                                    onChange={(e) => {
+                                        setStudent({
+                                            ...Student, phone: e.target.value
+                                        })
+                                    }}
+                                    className='form-control'
+                                    type="number"
+                                />
                             </p>
                         </Form.Item>
+
+                        <p>{t('direction')}:</p>
                         <Form.Item
                             rules={[
                                 {
@@ -259,9 +286,11 @@ function Ariza(props) {
                                     message: 'Yonalish tanlash kerak!'
                                 }
                             ]}
+                            name="changeSpecialtyID"
                         >
                             <Select
                                 placeholder="O'tmoqchi bo'lgan yo'nalishni tanlang..."
+                                name="changeSpecialtyID"
                                 style={{
                                     width: "100%",
                                 }}
@@ -269,6 +298,7 @@ function Ariza(props) {
                                 onChange={(e) => {
                                     setStudent({...Student, changeSpecialtyID: e})
                                 }}
+
                                 allowClear
                                 options={specialityList?.map((item) => {
                                     return {
@@ -287,7 +317,7 @@ function Ariza(props) {
                                     message: 'Talim shakli tanlanishi kerak!'
                                 }
                             ]}
-                            name="changeLanguage"
+                            name="newEducationForm"
 
                         >
                             <Select
@@ -296,7 +326,7 @@ function Ariza(props) {
                                     width: "100%",
                                 }}
                                 onChange={(e) => {
-                                    setStudent({...Student, educationForm: e})
+                                    setStudent({...Student, newEducationForm: e})
                                 }}
                                 allowClear
                                 options={specialityForms}
@@ -304,35 +334,87 @@ function Ariza(props) {
                         </Form.Item>
 
                         <p>{t('til')}:</p>
-                        <Form.Item>
+                        <Form.Item
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Ta\'lim tilini tanlash shart!'
+                                }
+                            ]}
+                            name="newEducationLang"
+                        >
                             <Select
                                 style={{
                                     width: "100%",
                                 }}
                                 placeholder="Ta'lim tilini tanlang..."
-                                onChange={handleChangeLen}
+                                onChange={(e) => {
+                                    setStudent({...Student, newEducationLang: e})
+                                }}
                                 allowClear
                                 options={specialityLang}
                             />
                         </Form.Item>
 
                         <p>Transklip / zachyotka</p>
-                        <input className='form-control' type="file" accept="application/pdf"/>
-                        <p>Sabab:
-                            <input className='mx-2' onChange={(e) => {
-                                setFileBoolin(e.target.checked)
-                            }}
-                                   type="checkbox"/>
-                            file
-                        </p>
-                        {
-                            fileBoolin ?
-                                <input className='form-control' type="file" accept="application/pdf"/>
-                                :
-                                <input className='form-control' type="text"/>
-                        }
+                        <Form.Item
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Transklip / zachyotka file tanlanishi kerak!'
+                                }
+                            ]}
+                            name="recordBookId"
+                        >
+                            <input
+                                className='form-control'
+                                type="file"
+                                accept="application/pdf"
+                            />
+                        </Form.Item>
+
+                        <Form.Item
+                            rules={[
+                                {
+                                    required: true,
+                                    message: `Sabab ${fileBoolin ? 'tanlanishi' : 'kiritilishi'}kerak!`
+                                }
+                            ]}
+                            name={fileBoolin ? "reasonFileId" : "reasonText"}
+                        >
+                            <p>Sabab:
+                                <input
+                                    className='mx-2'
+                                    onChange={(e) => {
+                                        setFileBoolin(e.target.checked)
+                                    }}
+                                    type="checkbox"
+                                />
+                                File
+                            </p>
+                            {
+                                fileBoolin ?
+                                    <input className='form-control' type="file" accept="application/pdf"/>
+                                    :
+                                    <input className='form-control' type="text"/>
+                            }
+                        </Form.Item>
                         <p>Pasport nusxasini yuklang</p>
-                        <input className='form-control' type="file" accept="application/pdf"/>
+                        <Form.Item
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Passport rasmini tanlang!'
+                                }
+                            ]}
+                            name="passportPhotoId"
+                        >
+                            <input
+                                className='form-control'
+                                type="file"
+                                accept="application/pdf"
+                            />
+                        </Form.Item>
 
 
                         <p>Ariza:</p>
@@ -436,6 +518,7 @@ function Ariza(props) {
             }
         }
     }
+
     return (
         <>
             <Nav/>
@@ -452,10 +535,10 @@ function Ariza(props) {
                             <p>{t('faculty')}: <span>{Student.faculty}</span></p>
                             <p>{t('direction')}: <span>{Student.specialty}</span></p>
 
-                            <p>{t('talim-shakli')}: <span>{Student.educationForm}</span></p>
-                            <p>{t('talim-tili')}: <span>{Student.educationLang}</span></p>
+                            <p>{t('talim-shakli')}: <span>{Student.oldEducationForm}</span></p>
+                            <p>{t('talim-tili')}: <span>{Student.oldEducationLang}</span></p>
 
-                            <p>{t('talim-turi')}: <span>{Student.educationType}</span></p>
+                            <p>{t('talim-turi')}: <span>{Student.oldEducationType}</span></p>
                             <p>{t('course')}: <span>{Student.course}</span></p>
 
                             <p>{t('group')}: <span>{Student.group}</span></p>
@@ -476,23 +559,25 @@ function Ariza(props) {
 
                         <div className="center">
                             <h5>{t('give-reason-for-ttj')}</h5>
-                            <Segmented onChange={(e) => {
-                                setStudent({...Student, applicationType: e})
-                            }} block options={[
-
-                                {
-                                    label: `${t('directionChang')}`,
-                                    value: "CHANGE_SPECIALITY"
-                                },
-                                {
-                                    label: `${t('recover')}`,
-                                    value: "RECOVER"
-                                },
-                                {
-                                    label: `${t('til')}`,
-                                    value: "CHANGE_LANG"
-                                },
-                            ]}/>
+                            <Segmented
+                                onChange={(e) => {
+                                    setStudent({...Student, applicationType: e})
+                                }}
+                                block
+                                options={[
+                                    {
+                                        label: `${t('directionChang')}`,
+                                        value: "CHANGE_SPECIALITY"
+                                    },
+                                    {
+                                        label: `${t('recover')}`,
+                                        value: "RECOVER"
+                                    },
+                                    {
+                                        label: `${t('til')}`,
+                                        value: "CHANGE_LANG"
+                                    },
+                                ]}/>
 
                             <h5 className="mt-4">TALABALAR UCHUN YO‘RIQNOMA</h5>
                             <p>
@@ -529,11 +614,8 @@ function Ariza(props) {
                         </div>
 
                         <div className="right-side overflow-auto">
-
-
                             <div className="container p-0">
                                 {showForm()}
-
                             </div>
                             <div className="d-flex justify-content-center">
                                 <Button loading={isLoading} className="signUp"
